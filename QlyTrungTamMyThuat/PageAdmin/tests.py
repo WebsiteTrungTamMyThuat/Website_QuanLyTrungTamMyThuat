0804fe445dd1db32ba3de244969e3fe901ff3ebc
Tran Hoang Vi <tranhoangvi21072003@gmail.com>
2024-11-19 00:33:25 +0700

    load giao vien

--- QlyTrungTamMyThuat/PageAdmin/tests.py
+++ QlyTrungTamMyThuat/PageAdmin/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from .models import GiaoVien
 # Create your tests here.
+
+class GiaoVienTestCase(TestCase):
+    def test_danh_sach_giao_vien(self):
+        
+        # Lấy tất cả giáo viên
+        dsgv = GiaoVien.objects.all()
+        print(dsgv.query)
+
+        # Kiểm tra số lượng giáo viên
+        self.assertEqual(dsgv.count(), 1)
