670976f4c31897af9157cfc743077021358a5380
nkd0123 <165612709+nkd0123@users.noreply.github.com>
2024-11-22 23:29:01 +0700

    22/11 met roi

--- PageUser/forms.py
+++ PageUser/forms.py
@@ -5,3 +5,14 @@ class HocVienForm(forms.ModelForm):
     class Meta:
         model = HocVien
         fields = ['mahv', 'hoten', 'email','SDT', 'GioiTinh', 'NgaySinh', 'DiaChi']
+        widgets = {
+            'hoten': forms.TextInput(attrs={'placeholder': 'Họ tên', 'class': 'input-field'}),
+            'SDT': forms.TextInput(attrs={'placeholder': 'Số điện thoại', 'class': 'input-field'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Email', 'class': 'input-field'}),
+            'NgaySinh': forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'input-field',
+                'placeholder': 'Ngày sinh',
+                'required': True
+            }),
+        }
